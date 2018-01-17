68b8e80245a39335586c9735d6b6c5d68d8abaa5
ikrtolica <igor@blinkinc.com>
2018-01-17 14:20:26 -0800

    Added test track call.

--- ios/SNC/AppDelegate.m
+++ ios/SNC/AppDelegate.m
@@ -22,6 +22,15 @@
 
   jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
 
+  SEGAnalyticsConfiguration *configuration = [SEGAnalyticsConfiguration configurationWithWriteKey:@"efbjlnxLEXD4pIChJQijwcQbscyb2zTE"];
+  configuration.trackApplicationLifecycleEvents = YES; // Enable this to record certain application events automatically!
+  configuration.recordScreenViews = YES; // Enable this to record screen views automatically!
+  [configuration use:[SEGFirebaseIntegrationFactory instance]];
+  [SEGAnalytics setupWithConfiguration:configuration];
+  
+  [[SEGAnalytics sharedAnalytics] track:@"Item Purchased"
+                             properties:@{ @"item": @"Sword of Heracles", @"revenue": @2.95 }];
+  
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
                                                       moduleName:@"SNC"
                                                initialProperties:nil
@@ -33,12 +42,6 @@
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
-  
-  SEGAnalyticsConfiguration *configuration = [SEGAnalyticsConfiguration configurationWithWriteKey:@"efbjlnxLEXD4pIChJQijwcQbscyb2zTE"];
-  configuration.trackApplicationLifecycleEvents = YES; // Enable this to record certain application events automatically!
-  configuration.recordScreenViews = YES; // Enable this to record screen views automatically!
-  [configuration use:[SEGFirebaseIntegrationFactory instance]];
-  [SEGAnalytics setupWithConfiguration:configuration];
   
   return YES;
 }
